14d98a369f697d4ec453bf614d9bffcd6a2c8dcd
Sergey-Zemba <sergey.zemba@gmail.com>
2015-11-06 01:26:16 +0200

    Friday, ‎November ‎6, ‎2015

--- SmartHouseWebForms/Default.aspx.cs
+++ SmartHouseWebForms/Default.aspx.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using SmartHouseWebForms.Controls;
 using SmartHouseWebForms.SmartHouse.Devices;
 using SmartHouseWebForms.SmartHouse.Interfaces;
 
